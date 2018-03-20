9c87ff67e517984dfe591e8ae81ce70bd377833b
johnsonDo <565045184@qq.com>
2018-03-20 18:21:28 +0800

    增加IO目录
    增加目录结构

--- src/main/java/study/IO/EncoderClass.java
+++ src/main/java/study/IO/EncoderClass.java
@@ -1,4 +1,4 @@
-package study;
+package study.IO;
 
 import java.io.UnsupportedEncodingException;
 
--- src/main/java/study/IO/FileClass.java
+++ src/main/java/study/IO/FileClass.java
@@ -1,4 +1,4 @@
-package study;
+package study.IO;
 
 import java.io.File;
 import java.io.IOException;
